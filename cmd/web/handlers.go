134ab0db15e24da8e9f3e06ac5b7f11269d64acf
nmdra <nimendradilshan11@gmail.com>
2024-11-26 01:22:18 +0530

    🎨 Complete HTML Template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	// "html/template"
 	"net/http"
 	"os"
 	"strconv"
-	"errors"
 
 	"github.com/nmdra/snipbox/internal/models"
 )
@@ -22,55 +21,39 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("server", hostname)
 
 	snippets, err := app.snippets.Latest()
-    if err != nil {
-        app.serverError(w, r, err)
-        return
-    }
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-    for _, snippet := range snippets {
-        fmt.Fprintf(w, "%+v\n", snippet)
-    }
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-	// files := []string{
-	// 	"./ui/html/base.tmpl",
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// }
-
-	// t, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
-
-	// err = t.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
-
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
 
 // View Snnipet Handeler
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.PathValue("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
 
-    snippet, err := app.snippets.Get(id)
-    if err != nil {
-        if errors.Is(err, models.ErrNoRecord) {
-            http.NotFound(w, r)
-        } else {
-            app.serverError(w, r, err)
-        }
-        return
-    }
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
 
-    // Write the snippet data as a plain-text HTTP response body.
-    fmt.Fprintf(w, "%+v", snippet)
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 // Snnipet Create
@@ -81,10 +64,10 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	// Dummy data TODO: remove this
 	title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := 7
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-	id, err := app.snippets.Insert(title,content,expires)
+	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
