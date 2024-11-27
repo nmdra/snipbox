c68e6fc36492d7503c7b016df6fec547b9338758
nmdra <nimendradilshan11@gmail.com>
2024-11-27 21:44:08 +0530

    🎨 Update

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/nmdra/snipbox/internal/models"
+	"github.com/nmdra/snipbox/internal/validator"
 )
 
 // Home Handler
@@ -51,20 +52,59 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Snnipet Create
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create Snippet..."))
+	data := app.newTemplateData(r)
+
+	data.Form = snippetCreateForm{
+        Expires: 365,
+    }
+
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
+}
+
+type snippetCreateForm struct {
+    Title       string
+    Content     string
+    Expires     int
+    validator.Validator
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Dummy data TODO: remove this
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := 7
-
-	id, err := app.snippets.Insert(title, content, expires)
+    err := r.ParseForm()
 	if err != nil {
-		app.serverError(w, r, err)
+		app.clientError(w, http.StatusBadRequest)
+        return
+	}
+
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+	form := snippetCreateForm{
+        Title:       r.PostForm.Get("title"),
+        Content:     r.PostForm.Get("content"),
+        Expires:     expires,
+        // FieldErrors: map[string]string{},
+    }
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+    form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+    form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+    form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
 		return
 	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+    if err != nil {
+        app.serverError(w, r, err)
+        return
+    }
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
