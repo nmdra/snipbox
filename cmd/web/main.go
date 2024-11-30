0cba665481de9a1d5dcb371651453846301ddfc3
nmdra <nimendradilshan11@gmail.com>
2024-11-30 12:40:37 +0530

    🚀 Update Database config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,8 +28,12 @@ type application struct {
 }
 
 func main() {
+
+	dbconn := &database{}
+	dbconn.setDBConnection()
+
 	var port = flag.String("port", "4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:pass@tcp(db:3306)/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", dbconn.connectionString(), "MySQL data source name")
 	flag.Parse()
 
 	// setup logger
